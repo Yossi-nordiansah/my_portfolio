64d9d11df9b50215e83153483d97f42ee68b8815
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-02-02 23:23:16 +0700

    activated pdf viewer

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,21 +1,17 @@
 import { useState } from "react";
 import foto from '../assets/img.png';
-import download from "../assets/icons/download.svg";
 import view from "../assets/icons/view.svg";
 import yayasan from "../assets/icons/course.svg";
 import education from "../assets/icons/education-cap-svgrepo-com.svg";
 import work from "../assets/icons/work.svg";
 import wa from '../assets/image/icons8-whatsapp-48.png';
 import { TypeAnimation } from 'react-type-animation';
+import {Document, Page} from "react-pdf";
 
 const Main = () => {
 
+    const pdfURL = 'https://drive.google.com/file/d/14qRNj7FRRGf4uO_BumBolqf8tzw0SXxM/view?usp=sharing';
     const [showContact, setShowContact] = useState(false);
-
-    const receiveData = (value) => {
-        setShowContact(true)
-    }
-
     return (
         <main className=" w-full min-w-full sm:pt-24 xs:pt-20 pt-16 bg-gradient-to-b grid grid-rows-[auto_auto_auto_auto_30px_auto] gap-y-3 from-blue-950 to-blue-800">
             <div className="relative">
@@ -34,25 +30,21 @@ const Main = () => {
                 speed={50}
                 cursor={false}
                 repeat={0}
-                className="absolute font-kanit lg:text-4xl sm:text-4xl sm:text-left xs:text-[18px] xs:leading-[28px] text-[13px] xl:pl-32 lg:pl-14 md:pl-10 sm:pl-8 xs:pl-4 pl-3 text-center text-sky-400 tracking-wider font-bold drop-shadow-[0_0px_15px_#0066ff]"
+                className="absolute font-kanit lg:text-4xl sm:text-4xl sm:text-left xs:text-[22px] xs:leading-[28px] text-[18px] xl:pl-32 lg:pl-14 md:pl-10 sm:pl-8 xs:pl-5 pl-5  text-sky-400 tracking-wider font-bold drop-shadow-[0_0px_15px_#0066ff]"
             />
             </div>
 
             {/* <!-- intro --> */}
-            <div className="xl:w-4/5 lg:w-[90%] mx-auto lg:mt-16 sm:mt-14 xs:mt-10 mt-5 flex lg:flex-row flex-col-reverse justify-between items-center" id="about">
+            <div className="xl:w-4/5 lg:w-[90%] mx-auto lg:mt-16 sm:mt-14 xs:mt-14 mt-10 flex lg:flex-row flex-col-reverse justify-between items-center" id="about">
                 <div className="lg:w-8/12 w-[90%] ">
                     <p className="font-inter sm:text-xl xs:text-lg text-sm lg:w-full md:w-full xs:font-semibold sm:first-letter:text-6xl xs:first-letter:text-4xl xxs:first-letter:text-2xl text-slate-200 text-justify">
                         Hello, My name is <span className="text-blue-400 font-poppins">Yossi Nordiansah</span>, I am a Junior Front-End Web Developer who works with React.js Library or Vanilla JavaScript.  I am also accustomed to using Git and GitHub for Collaborative teamwork. Currently, I reside in the city of Mojokerto and am an active student at one of the private universities located there. At the moment, I have gained experience working on various projects, ranging from simple to mid-scale websites, with Attractive and Responsive designs.
                     </p>
                     <div className="mt-7 flex gap-3">
-                        <div className="border-2 border-dashed flex items-center justify-center gap-2 hover:bg-stone-500 rounded-lg sm:w-[30%] xs:w-[40%] w-[60%] cursor-pointer bg-white/30 border-yellow-400 sm:py-1 py-2">
-                            <img src={download} className='sm:w-6 w-4' alt="" />
-                            <p className="text-white md:text-sm sm:text-[12px] xs:text-[10px] text-[8px]">DOWNLOAD RESUME</p>
-                        </div>
-                        <div className="border-2 border-dashed hover:bg-stone-500 flex items-center gap-2 justify-center rounded-lg sm:w-[30%] xs:w-[40%] w-[60%] cursor-pointer bg-white/30 border-yellow-400 py-1">
+                        <a href={pdfURL} target="_blank" download="Yossi Nordiansah Resume" className="border-2 border-dashed hover:bg-stone-500 flex items-center gap-2 justify-center rounded-lg w-fit cursor-pointer bg-white/30 border-yellow-400 py-1 px-3">
                             <img src={view} className='sm:w-6 w-4 ' alt="" />
                             <p className="text-white md:text-sm sm:text-[12px] xs:text-[10px] text-[8px] text-center">VIEW RESUME</p>
-                        </div>
+                        </a>
                     </div>
                 </div>
                 <div className="border-8 border-blue-300 overflow-hidden rounded-full drop-shadow-[0_0px_15px_#0066ff] sm:h-72 sm:w-72 w-56 h-56 ">
@@ -61,8 +53,8 @@ const Main = () => {
             </div>
 
             {/* <!-- Education --> */}
-            <div className="w-4/5 mt-20 m-auto justify-between mobile-sm:flex-col laptop:flex-row items-center" id="education">
-                <h1 className=" font-poppins mobile-sm:pl-4 laptop:pl-0 font-bold text-4xl text-blue-300 text-left">Education</h1>
+            <div className="xl:w-4/5 w-[90%] mt-20 m-auto justify-between mobile-sm:flex-col laptop:flex-row items-center" id="education">
+                <h1 className=" font-poppins font-bold text-4xl text-blue-300 text-left">Education</h1>
                 <div className="lg:pl-6 mt-10">
                     <img src={yayasan} alt="" className="w-6" />
                     <div className="border-l-2 border-white ml-3 mt-2 xs:h-20 h-28 leading-5">
@@ -93,7 +85,7 @@ const Main = () => {
             </div>
 
             {/* <!-- Work Experience --> */}
-            <div className="w-4/5 mt-20 mx-auto justify-between " id="education">
+            <div className="xl:w-4/5 w-[90%] mt-20 mx-auto justify-between " id="education">
                 <h1 className="font-poppins pl-0 font-bold xs:text-4xl text-3xl text-blue-300 text-left">Work Experience</h1>
                 <div className="lg:pl-6 mt-10">
                     <img src={work} alt="" className="w-6" />
@@ -128,7 +120,6 @@ const Main = () => {
             <div className="fixed md:hidden right-6 bottom-9">
                 <a target="_blank" href="https://wa.me/6285655230897"><img src={wa} alt="" /></a>
             </div>
-
         </main>
     )
 }
