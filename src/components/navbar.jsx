1d6b014480f40e9dade0dacc3f6cbf90ddf1eb1d
Yossi-nordiansah <yossi.nordiansah99@gmail.com>
2024-02-09 16:01:24 +0700

    tes onfocus

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -31,21 +31,29 @@ const Navbar = () => {
         }
     ]);
 
-    useEffect(()=>{
+    useEffect(() => {
         navbarMenu.map(item => {
             if (item.status === 'clicked') {
                 navigate(item.path)
             }
         })
 
-        
-    },[])
+
+    }, [])
+
+    useEffect(()=>{
+        if(showMenu === true){
+            console.log('true')
+        }else{
+            console.log('false')
+        }
+    },[showMenu])
 
     const handleOnclickMenu = (id) => {
         if (navbarMenu.find((items) => items.id === id)) {
             setNavbarMenu(navbarMenu.map((item) => (item.id === id ? { ...item, status: "clicked" } : { ...item, status: "unclicked" })));
         }
-    }
+    };
 
     return (
         <nav className="sm:px-10 px-5  bg-blue-800/5 w-full py-2 fixed flex justify-between items-center backdrop-blur-md z-10 ">
@@ -53,23 +61,33 @@ const Navbar = () => {
             <ul className="text-white font-bold font-inter gap-8 hidden md:flex">
                 {navbarMenu?.map(item =>
                     item.status === 'clicked' ?
-                        <li key={item.id} onClick={()=>handleOnclickMenu(item.id)} className="hover:text-blue-400 text-blue-400 shrink-0 drop-shadow-[0_0px_15px_#0073e6] hover:drop-shadow-[0_0px_15px_#0073e6]" title={item.name}><Link to={item.path}>{item.name}</Link></li> : 
-                        <li key={item.id} onClick={()=>handleOnclickMenu(item.id)} className="" title={item.name}><Link to={item.path}>{item.name}</Link></li>
+                        <li key={item.id} onClick={() => handleOnclickMenu(item.id)} className="hover:text-blue-400 text-blue-400 shrink-0 drop-shadow-[0_0px_15px_#0073e6] hover:drop-shadow-[0_0px_15px_#0073e6]" title={item.name}><Link to={item.path}>{item.name}</Link></li> :
+                        <li key={item.id} onClick={() => handleOnclickMenu(item.id)} className="" title={item.name}><Link to={item.path}>{item.name}</Link></li>
                 )}
             </ul>
-            <div className="relative md:hidden">
-            <img src={menu} alt="" onClick={()=>setShowMenu(!showMenu)} className={`md:hidden ${showMenu? "hidden" : "block"}`}/>
-            {showMenu &&  <img src={menuClose} alt="" onClick={()=>setShowMenu(!showMenu)} className={`md:hidden`}/>}
-            </div>
-                <ul className={`bg-blue-800 ${showMenu? 'block' : 'hidden'} border-zinc-400 border-2 font-semibold rounded-lg absolute text-white right-10 top-10 sm:w-[30%] xs:w-[40%] w-[50%] text-center`}>
-                    {
-                        navbarMenu?.map(item =>(  
-                                <Link to={item.path} key={item.id}>
-                                    <li  className={`py-3 border-b active:bg-blue-900`} key={item.id}>{item.name}</li>
-                                </Link>
-                        ))
-                    }
-                </ul>
+            {/* <button className="relative md:hidden border">
+                <img src={menu} alt="" onClick={() => setShowMenu(!showMenu)} className={`md:hidden ${showMenu ? "hidden" : "block"}`} />
+                {showMenu && <img src={menuClose} alt="" onClick={() => setShowMenu(!showMenu)} className={`md:hidden`} />}
+                <img src={menu} alt="" tabIndex={1} onFocus={() => setShowMenu(true)} className={`md:hidden ${showMenu ? "hidden" : "block"}`} 
+                 onBlur={() => handleBlur}/>
+                {showMenu && <img src={menuClose} alt="" onClick={() => setShowMenu(false)} className={`md:hidden`} />}
+            </button> */}
+            <button className="relative md:hidden border-none" onFocus={() => setShowMenu(true)}  onBlur={() => setShowMenu(false)}>
+                {/* <img src={menu} alt="" onClick={() => setShowMenu(!showMenu)} className={`md:hidden ${showMenu ? "hidden" : "block"}`} />
+                {showMenu && <img src={menuClose} alt="" onClick={() => setShowMenu(!showMenu)} className={`md:hidden`} />} */}
+                <img src={menu} alt="" className={`md:hidden ${showMenu ? "hidden" : "block"}`} 
+                />
+                {showMenu && <img src={menuClose} alt="" onClick={() => setShowMenu(false)} className={`md:hidden`} />}
+            </button>
+            <ul className={`bg-blue-800 ${showMenu ? 'block' : 'hidden'} border-zinc-400 border-2 font-semibold rounded-lg absolute text-white right-10 top-10 sm:w-[30%] xs:w-[40%] w-[50%] text-center`}>
+                {
+                    navbarMenu?.map(item => (
+                        <Link to={item.path} key={item.id}>
+                            <li className={`py-3 border-b active:bg-blue-900`} key={item.id}>{item.name}</li>
+                        </Link>
+                    ))
+                }
+            </ul>
         </nav>
     )
 }
